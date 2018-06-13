7fac800d7c6e06580646699f2cd27bddc97e9789
proforty7 <proforty7@gmail.com>
2018-06-13 23:05:49 +0530

    Added intent

--- app/src/main/java/com/example/prateek/swoosh/welcomeActivity.kt
+++ app/src/main/java/com/example/prateek/swoosh/welcomeActivity.kt
@@ -1,12 +1,19 @@
 package com.example.prateek.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class welcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getstartedbtn.setOnClickListener {
+            val leagueIntent = Intent(this, leagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
