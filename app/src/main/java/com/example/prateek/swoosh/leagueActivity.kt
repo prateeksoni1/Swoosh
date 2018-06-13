53a4c0fe86bf5376cffb042b7e652f19e13516a6
proforty7 <proforty7@gmail.com>
2018-06-13 22:40:46 +0530

    Added League Layout

---
+++ app/src/main/java/com/example/prateek/swoosh/leagueActivity.kt
@@ -0,0 +1,12 @@
+package com.example.prateek.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class leagueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
