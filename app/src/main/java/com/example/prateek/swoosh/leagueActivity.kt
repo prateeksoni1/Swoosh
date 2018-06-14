32b9ff447cc55023d15770a8a2bd85c48632724d
proforty7 <proforty7@gmail.com>
2018-06-15 01:55:40 +0530

    Added another layout

--- app/src/main/java/com/example/prateek/swoosh/leagueActivity.kt
+++ app/src/main/java/com/example/prateek/swoosh/leagueActivity.kt
@@ -1,12 +1,21 @@
 package com.example.prateek.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class leagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+
+    }
+
+    fun nextBtnClicked(view: View) {
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
