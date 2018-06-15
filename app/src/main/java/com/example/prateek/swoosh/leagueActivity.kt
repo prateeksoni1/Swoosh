dc315eb641a7c0d1897c37296818636ebd05d626
proforty7 <proforty7@gmail.com>
2018-06-15 12:04:54 +0530

    added buttons

--- app/src/main/java/com/example/prateek/swoosh/leagueActivity.kt
+++ app/src/main/java/com/example/prateek/swoosh/leagueActivity.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class leagueActivity : AppCompatActivity() {
+
+    var leagueSelected = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +18,37 @@ class leagueActivity : AppCompatActivity() {
 
     }
 
+    fun onMensBtnClicked (view: View) {
+
+        womensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+        leagueSelected = "mens"
+    }
+
+    fun onWomensBtnClicked (view: View) {
+
+        mensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+        leagueSelected = "womens"
+
+    }
+
+    fun onCoedBtnClicked  (view: View) {
+
+        mensLeagueBtn.isChecked = false
+        womensLeagueBtn.isChecked = true
+        leagueSelected = "coed"
+
+    }
+
     fun nextBtnClicked(view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+
+        if(leagueSelected == "") {
+            Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
+        } else {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            startActivity(skillActivity)
+        }
+
     }
 }
